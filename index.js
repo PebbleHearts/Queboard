f150e1348f0ec930e9b4b19c1c8a75bcc36c099d
Nirmal <nirmal@lm-kv-nirmal-michel.local>
2025-06-06 15:10:31 +0530

    feat: create the basic functionality for pasting the items in the order in which they are copied

---
+++ index.js
@@ -0,0 +1,49 @@
+import { GlobalKeyboardListener } from 'node-global-key-listener';
+import clipboardy from 'clipboardy';
+import Queue from './queue.js';
+
+const keyboardListener = new GlobalKeyboardListener();
+
+let listening = false;
+const queuedClipboardHistory = new Queue();
+
+const handleCopyShortcutExecuted = async () => {
+  const current = await clipboardy.read();
+
+  queuedClipboardHistory.enqueue(current);
+
+  const firstItem = queuedClipboardHistory.peek();
+  if (firstItem) {
+    await clipboardy.write(firstItem);
+  }
+}
+
+const handlePasteShortcutExecuted = async () => {
+  queuedClipboardHistory.dequeue();
+  const firstItem = queuedClipboardHistory.peek();
+  if (firstItem) {
+    await clipboardy.write(firstItem);
+  }
+}
+
+keyboardListener.addListener(function (e, down) {
+  const downKeys = down ? Object.keys(down).filter(key => down[key]) : [];
+  if (downKeys.length === 3 && downKeys.includes('LEFT META') && downKeys.includes('LEFT SHIFT') && downKeys.includes('L')) {
+    listening = !listening;
+    if (listening) {
+      console.log('clipboard manager: started');
+      queuedClipboardHistory.reset();
+    } else {
+      console.log('clipboard manager: stopped');
+      queuedClipboardHistory.reset();
+    }
+  }
+
+  if (listening && downKeys.length === 2 && downKeys.includes('LEFT META')) {
+    if (downKeys.includes('C')) {
+      handleCopyShortcutExecuted();
+    } else if (downKeys.includes('V')) {
+      handlePasteShortcutExecuted();
+    }
+  }
+});
